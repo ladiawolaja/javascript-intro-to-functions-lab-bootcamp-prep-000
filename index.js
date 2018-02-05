e4c0ac1856158a581d966582d5ba6c15f266a807
Samson Awolaja <samson.awolaja@gmail.com>
2018-02-05 17:25:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,37 @@
+function shout(string) {
+  // receives one argument and returns it in all caps
+  return string.toUpperCase()
+}
+'Hello'.toUpperCase()
+
+function whisper(string) {
+  // receives one argument and returns it in all lowercase.
+  return string.toLowerCase()
+}
+'Samson'.toUpperCase()
+
+function logShout(string) {
+  // calls console.log() its one argument in all caps.
+  console.log('HELLO'.toUpperCase())
+}
+'HELLO'
+
+function logWhisper(string) {
+  //calls console.log() its one argument in all lowercase.
+  console.log('hello'.toLowerCase())
+}
+'hello'
+
+function sayHiToGrandma(string) {
+  var lowercase = "hello!"
+  if (lowercase.toLowerCase()=== lowercase) {
+    
+  }
+  return `I can't hear you!`
+}
+'hello!'.toLowerCase
+
+function sayHiToGrandma(string) {
+  return `I love you, too.`
+}
+"I love you, Grandma"
